161cbd9d58e6a039ae8d0bae89d1baede4ee81aa
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-11-11 17:06:14 -0600

    Update test.ts

--- src/slashcommands/test.ts
+++ src/slashcommands/test.ts
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 export default {
-  data: new SlashCommandBuilder().setName('test').setDescription('test'),
+  data: new SlashCommandBuilder().setName('test2').setDescription('test2'),
   execute: async (interaction: any) => {
     await interaction.reply('you used a slash command');
   }
